8d4a1b94b6857c09f6b9d6599385272221d668c1
valmauricio <val_mauricio@hotmail.com>
2022-10-30 17:26:38 -0300

    asd

--- js/cart.js
+++ js/cart.js
@@ -1,46 +1,45 @@
-const radioH1 = document.getElementById('r1');
-const radioH2 = document.getElementById('r2');
-const inputZero = document.getElementsByName('zero');
+const radioH1 = document.getElementById("r1");
+const radioH2 = document.getElementById("r2");
+const inputZero = document.getElementsByName("zero");
+
 
 (function () {
-  'use strict'
+  "use strict";
 
   // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll(".needs-validation");
 
   // Bucle sobre ellos y evitar el envío
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
 
-        form.classList.add('was-validated')
-        
+        form.classList.add("was-validated");
 
-
-if (!radioH1.checked && !radioH2.checked) {
-  document.getElementById('but').innerHTML = 'Debe seleccionar una forma de pago'
-  
-  
- 
-}     
-      }, false)
-    })
-})()
+        if (!radioH1.checked && !radioH2.checked) {
+          document.getElementById("but").innerHTML =
+            "Debe seleccionar una forma de pago";
+            
+        } 
+      },
+      false
+    );
+  });
+})();
 
 function Zero() {
   for (let i = 0; i < inputZero.length; i++) {
     let input = inputZero[i];
-  if ((input.value == 0)) {
-    input.value = 1; 
-  } 
-  } 
-  
+    if (input.value == 0) {
+      input.value = 1;
+    }
+  }
 }
-
 
 function cerrar() {
   Swal.fire({
@@ -58,20 +57,18 @@ function cerrar() {
       localStorage.removeItem("user");
       localStorage.removeItem("cartArray");
       location.href = "login.html";
-      
     }
   });
 }
 
-function deleteCart(){
-  localStorage.removeItem('cartArray');
+function deleteCart() {
+  localStorage.removeItem("cartArray");
   cartInfo = [];
-  document.getElementById("cartinfo").innerHTML = ""
-  final()
-  
+  document.getElementById("cartinfo").innerHTML = "";
+  final();
 }
 
-function valor(numb,par2,par3) {
+function valor(numb, par2, par3) {
   document.getElementById(par3).innerHTML =
     numb * document.getElementById(par2).value;
 }
@@ -79,124 +76,127 @@ function valor(numb,par2,par3) {
 // function payMethod() {
 //   Swal.fire({
 //     title: '<strong>Forma de pago</strong><hr>',
-    
+
 //     html:'<div class="container"><input onchange="desabilitar()" type="radio" id="r1" name="choose" value="Tarjeta de credito">Tarjeta de credito<hr></div>' +
 //     '<div class="container row"><div class="col-6"><label>Numero de tarjeta</label><div class="form-group has-feedback" style="text-align: left;"><i class="fa fa-credit-card form-control-feedback" aria-hidden="true"></i><input id="r10" class="form-control" type="number"></div><br><label>Vencimiento (MM/AA)<label><input id="r11" class="form-control" type="date"></div><div class="col"><label>Codigo de seguridad</label><div class="form-group has-feedback" style="text-align: left;"><i class="fa fa-lock form-control-feedback" aria-hidden="true"></i><input id="r12" style="width: 50%;" class="form-control" type="number"></div></div></div><br>'+
 //     '<hr><div class="container"><input type="radio" onchange="desabilitar()" id="r2" name="choose" value="Transferencia bancaria"> Transferencia bancaria <hr> <label class="form-label" for="r20">Numero de cuenta</label><div class="form-group has-feedback" style="text-align: left;"><i class="fa fa-id-card form-control-feedback" aria-hidden="true"></i><input id="r20" class="form-control r2" type="number"></div></div><hr>'+
 //     '<button class="btn btn-info" id="select" type="button">Guardar seleccion</button>',
 //     showCloseButton: false,
 //     showCancelButton: false,
-    
+
 //     confirmButtonText:
 //       'Cerrar',
-    
-   
+
 //   })
 //   document.getElementById('select').addEventListener("click", () => {
 //     selectPay();
-    
+
 //   });
 
 // } ESTO ERA CON SWEET ALERT
-function selectEnv(){
-  const radios = document.getElementsByName('radioenv');
-  const sub = document.getElementById('productCostTotal').innerHTML;
-let radioResult= 0;
-for (var i = 0; i <  radios.length; i++) {
-  if (radios[i].checked) {
-    radioResult = radios[i].value
-    
-    document.getElementById('comissionText').innerHTML = ((parseFloat(radios[i].value) * parseFloat(sub))/ 100).toFixed(2)
-    break;
-  }
-}
-}
+
 function selectPay() {
-  const radios = document.getElementsByName('choose');
+  const radios = document.getElementsByName("choose");
 
-for (var i = 0; i <  radios.length; i++) {
-  if (radios[i].checked) {
-    document.getElementById('selected').innerHTML = radios[i].value
-    
-    break;
-   
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      document.getElementById("selected").innerHTML = radios[i].value;
+
+      break;
+    }
   }
-  
-}
-document.getElementById('but').innerHTML = ''
+  document.getElementById("but").innerHTML = "";
 }
 
-function desabilitar()
-{
-    var radio1 = document.getElementById('r1').checked;
-    var inputs = document.getElementById('r20');
-    if (radio1 === true) {
-      inputs.value = ''
-        inputs.disabled = true;
-        
-    } else if (radio1 === false) {
-        inputs.disabled = false;
-    }
-    var radio2 = document.getElementById('r2').checked;
-    var inputs2 = document.getElementById('r10');
-    var inputs3 = document.getElementById('r11');
-    var inputs4 = document.getElementById('r12');
-    if (radio2 === true) {
-      inputs2.value = ''
-        inputs2.disabled = true;
-        inputs3.value = ''
-        inputs3.disabled = true;
-        inputs4.value = ''
-        inputs4.disabled = true;
-        
-    } else if (radio2 === false) {
-        inputs2.disabled = false;
-        inputs3.disabled = false;
-        inputs4.disabled = false;
-    }
 
+function final() {
+  subTotal();
+  selectEnv();
+  total();
 }
-function final(){
-subTotal()
-selectEnv()
-total()
-console.log(JSON.parse(localStorage.getItem("cartArray")))
-}
-function total(){
-  let sub = document.getElementById('productCostTotal').innerHTML
-  let env = document.getElementById('comissionText').innerHTML
-  document.getElementById('totalCostText').innerHTML = (parseFloat(sub) + parseFloat(env)).toFixed(2)
-}
+// PAUTA 1/6
+// -----------------------------------------------------------------------------------------------------------------------------------
 function subTotal() {
-  const dolares = document.getElementsByClassName('USD')
-  const pesos = document.getElementsByClassName('UYU')
+  const dolares = document.getElementsByClassName("USD");
+  const pesos = document.getElementsByClassName("UYU");
   let dol = 0;
   let pes = 0;
-  for (let i = 0; i <  dolares.length; i++) {   
-     dol += parseFloat(dolares[i].innerHTML);
+  for (let i = 0; i < dolares.length; i++) {
+    dol += parseFloat(dolares[i].innerHTML);
   }
-  for (let i = 0; i <  pesos.length; i++) {   
-    pes += parseFloat(pesos[i].innerHTML);  
- }
- document.getElementById("productCostTotal") .innerHTML = ((pes / 40) + dol).toFixed(2)
+  for (let i = 0; i < pesos.length; i++) {
+    pes += parseFloat(pesos[i].innerHTML);
+  }
+  document.getElementById("productCostTotal").innerHTML = (
+    pes / 40 +
+    dol
+  ).toFixed(2);
 }
+function selectEnv() {
+  const radios = document.getElementsByName("radioenv");
+  const sub = document.getElementById("productCostTotal").innerHTML;
+  let radioResult = 0;
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      radioResult = radios[i].value;
 
-
-function arrayDeleted(nombre){
-  let deleted = cartInfo.filter(item => item.name !== nombre)
-// let deleted = deleted1.filter(item => item.name !== 'Peugeot 208')
+      document.getElementById("comissionText").innerHTML = (
+        (parseFloat(radioResult) * parseFloat(sub)) /
+        100
+      ).toFixed(2);
+      break;
+    }
+  }
+}
+function total() {
+  let sub = document.getElementById("productCostTotal").innerHTML;
+  let env = document.getElementById("comissionText").innerHTML;
+  document.getElementById("totalCostText").innerHTML = (
+    parseFloat(sub) + parseFloat(env)
+  ).toFixed(2);
+}
+// --------------------------------------------------------------------------------------------------------------------------
+// PAUTA 2/6
+function desabilitar() {
+  var radio1 = document.getElementById("r1").checked;
+  var inputs = document.getElementById("r20");
+  if (radio1 === true) {
+    inputs.value = "";
+    inputs.disabled = true;
+  } else if (radio1 === false) {
+    inputs.disabled = false;
+  }
+  var radio2 = document.getElementById("r2").checked;
+  var inputs2 = document.getElementById("r10");
+  var inputs3 = document.getElementById("r11");
+  var inputs4 = document.getElementById("r12");
+  if (radio2 === true) {
+    inputs2.value = "";
+    inputs2.disabled = true;
+    inputs3.value = "";
+    inputs3.disabled = true;
+    inputs4.value = "";
+    inputs4.disabled = true;
+  } else if (radio2 === false) {
+    inputs2.disabled = false;
+    inputs3.disabled = false;
+    inputs4.disabled = false;
+  }
+}
+// -----------------------------------------------------------------------------------------------------------------
+function arrayDeleted(nombre) {
+  let deleted = cartInfo.filter((item) => item.name !== nombre);
   
-  localStorage.removeItem('cartArray');
+
+  localStorage.removeItem("cartArray");
   cartInfo = deleted;
-  localStorage.setItem('cartArray',JSON.stringify(deleted))
+  localStorage.setItem("cartArray", JSON.stringify(deleted));
 }
-function deleteOne(id){
-  
-     let element = document.getElementById(id);
-     return element.parentNode.removeChild(element);
-   }
+function deleteOne(id) {
+  let element = document.getElementById(id);
+  return element.parentNode.removeChild(element);
+}
 function showCartInfo() {
-  
   let cartissues = "";
 
   for (let i = 0; i < cartInfo.length; i++) {
@@ -209,21 +209,16 @@ function showCartInfo() {
 <td class="col-2">${com.name}</td>
 <td class="col-2">${com.currency + " " + com.unitCost}</td>
 <td class="col-2"><input id="${[i]}" type="number"  class="form-control" name="zero" value=1 min=1 onkeyup="Zero(), valor(${
-  com.unitCost
-},'${[i]}','${[i]}w')" onchange="valor(${
       com.unitCost
-    },'${[i]}','${[i]}w'), subTotal(), selectEnv(), total()" style="width: 5em;" required></input></td>
+    },'${[i]}','${[i]}w')" onchange="valor(${com.unitCost},'${[i]}','${[i]}w'), subTotal(), selectEnv(), total()" style="width: 5em;" required></input></td>
 <td class="col-2"><b>${com.currency + " "}<span class="${com.currency}" id="${[i]}w"> ${
       com.unitCost * com.count
-    }</span></b></td><td><button class="btn btn-danger" onclick="deleteOne('${[i]}del'), final(), arrayDeleted('${com.name}')"><i class="fa fa-trash" aria-hidden="true"></i>
+    }</span></b></td><td><button class="btn btn-danger" onclick="deleteOne('${[i]}del'), final(), arrayDeleted('${
+      com.name}')"><i class="fa fa-trash" aria-hidden="true"></i>
     </button></td>
 </tr>`;
-
   }
   document.getElementById("cartinfo").innerHTML += cartissues;
-  
-  
-  
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -251,57 +246,43 @@ document.addEventListener("DOMContentLoaded", function (e) {
   });
 
   getJSONData(CART_INFO_URL + 25801 + EXT_TYPE).then(function (resultObj) {
-    if (resultObj.status === "ok" ) {
+    if (resultObj.status === "ok") {
       
-      // if 
-      // // (JSON.parse(localStorage.getItem("cartArray")) == null)
-      //  {
-        
-      // } else {
-      //   // JSON.parse(localStorage.getItem("cartArray")).find((object) => {
-      //   //   cartInfo.push(object);
-      //   // });
-      // }
-      
-      
-    
-    if (JSON.parse(localStorage.getItem("cartArray")) == null || JSON.parse(localStorage.getItem("cartArray")).length == 0){
-      cartInfo = resultObj.data.articles
-      
-      
-    } else {
-      cartInfo = [];
-      JSON.parse(localStorage.getItem("cartArray")).find((object) => {
-        cartInfo.push(object);
-        
-      });}
-      
-
+      if (
+        JSON.parse(localStorage.getItem("cartArray")) == null ||
+        JSON.parse(localStorage.getItem("cartArray")).length == 0
+      ) {
+        cartInfo = resultObj.data.articles;
+      } else {
+        cartInfo = [];
+        JSON.parse(localStorage.getItem("cartArray")).find((object) => {
+          cartInfo.push(object);
+        });
+      }
     }
     showCartInfo();
-    subTotal()
-    selectEnv()
-    total()
+    subTotal();
+    selectEnv();
+    total();
     selectPay();
-    desabilitar()
-  
+    desabilitar();
   });
 
-  document.getElementById('deletec').addEventListener("click", () => {
+  document.getElementById("deletec").addEventListener("click", () => {
     deleteCart();
   });
   // document.getElementById('modal').addEventListener("click", () => {
   //   payMethod();
   // }); ESTO ERA CON SWEET ALERT
-  document.getElementById('select').addEventListener("click", () => {
+  document.getElementById("select").addEventListener("click", () => {
     selectPay();
+    
     Swal.fire({
-      position: 'top-end',
-      icon: 'success',
-      title: 'Datos almacenados',
+      position: "top-end",
+      icon: "success",
+      title: "Datos almacenados",
       showConfirmButton: false,
-      timer: 1500
-    })
+      timer: 1500,
+    });
   });
-  
 });
